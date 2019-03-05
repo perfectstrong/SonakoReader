ccb3ba4bfac53db32162164b03d69166b6071699
perfectstrong <ilove1q@yahoo.com.vn>
2019-03-05 15:42:01 +0100

    Make page viewer zoomable

--- app/src/main/java/perfectstrong/sonako/sonakoreader/PageReadingActivity.java
+++ app/src/main/java/perfectstrong/sonako/sonakoreader/PageReadingActivity.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MotionEvent;
 import android.webkit.WebView;
 
 import perfectstrong.sonako.sonakoreader.helper.Config;
@@ -35,9 +34,10 @@ public class PageReadingActivity extends AppCompatActivity {
         pageview = findViewById(R.id.page_viewer);
         pageview.setInitialScale(1);
         pageview.setScrollContainer(false);
-        pageview.setOnTouchListener((v, event) -> (event.getAction() == MotionEvent.ACTION_MOVE));
+//        pageview.setOnTouchListener((v, event) -> (event.getAction() == MotionEvent.ACTION_MOVE));
         pageview.getSettings().setJavaScriptEnabled(false);
         pageview.getSettings().setLoadWithOverviewMode(true);
         pageview.getSettings().setUseWideViewPort(true);
+        pageview.getSettings().setBuiltInZoomControls(true);
     }
 }
