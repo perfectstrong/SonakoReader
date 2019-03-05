4112b30addf0f5725b79b724f67e3ec40ee23a94
perfectstrong <ilove1q@yahoo.com.vn>
2019-03-05 16:23:00 +0100

    Add basic popup menu

--- app/src/main/java/perfectstrong/sonako/sonakoreader/MainActivity.java
+++ app/src/main/java/perfectstrong/sonako/sonakoreader/MainActivity.java
@@ -69,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        getMenuInflater().inflate(R.menu.main_activity_menu_scrolling, menu);
         return true;
     }
 
--- app/src/main/java/perfectstrong/sonako/sonakoreader/PageReadingActivity.java
+++ app/src/main/java/perfectstrong/sonako/sonakoreader/PageReadingActivity.java
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 
 import perfectstrong.sonako.sonakoreader.helper.Config;
@@ -39,5 +41,29 @@ public class PageReadingActivity extends AppCompatActivity {
         pageview.getSettings().setLoadWithOverviewMode(true);
         pageview.getSettings().setUseWideViewPort(true);
         pageview.getSettings().setBuiltInZoomControls(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.page_reading_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh_page:
+                // TODO refresh page
+                break;
+            case R.id.action_purge_cache_page:
+                // TODO purge cache
+                break;
+            case R.id.action_settings:
+                // TODO change settings
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
