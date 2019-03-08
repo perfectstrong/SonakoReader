eb2dfb77a5850394224f3830596c6241908ea3ec
perfectstrong <ilove1q@yahoo.com.vn>
2019-03-08 15:14:12 +0100

    Suppress access warning

--- app/src/main/java/perfectstrong/sonako/sonakoreader/database/TimestampConverter.java
+++ app/src/main/java/perfectstrong/sonako/sonakoreader/database/TimestampConverter.java
@@ -4,14 +4,15 @@ import android.arch.persistence.room.TypeConverter;
 
 import java.util.Date;
 
+@SuppressWarnings("WeakerAccess")
 public class TimestampConverter {
     @TypeConverter
-    Date fromTimestamp(Long value) {
+    public Date fromTimestamp(Long value) {
         return new Date(value);
     }
 
     @TypeConverter
-    Long fromDate(Date value) {
+    public Long fromDate(Date value) {
         return value.getTime();
     }
 }
