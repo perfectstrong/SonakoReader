a9bc6c6186900f9784e0cafde903e911195a12b1
perfectstrong <ilove1q@yahoo.com.vn>
2019-03-10 08:59:13 +0100

    Generify

--- app/src/main/java/perfectstrong/sonako/sonakoreader/fragments/LNShowcaseAdapter.java
+++ app/src/main/java/perfectstrong/sonako/sonakoreader/fragments/LNShowcaseAdapter.java
@@ -13,8 +13,6 @@ import android.view.ViewGroup;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import perfectstrong.sonako.sonakoreader.PageReadingActivity;
@@ -24,33 +22,10 @@ import perfectstrong.sonako.sonakoreader.database.LightNovel;
 import perfectstrong.sonako.sonakoreader.database.LightNovelsDatabaseClient;
 import perfectstrong.sonako.sonakoreader.helper.Config;
 
-public class LNShowcaseAdapter extends RecyclerView.Adapter<LNShowcaseAdapter.LNTitleViewHolder> {
+public class LNShowcaseAdapter extends LNListAdapter<LNShowcaseAdapter.LNTitleViewHolder> {
     private Context context;
-    private List<LightNovel> _lnList = new ArrayList<>();
-    private List<LightNovel> lnList = new ArrayList<>();
-
     LNShowcaseAdapter(Context context) {
         this.context = context;
-    }
-
-    public void setDatalist(List<LightNovel> titles) {
-        _lnList.addAll(titles);
-        loadByChunk(_lnList);
-    }
-
-    private void loadByChunk(List<LightNovel> list) {
-        lnList.clear();
-        notifyDataSetChanged();
-        int currentIndex = 0;
-        int chunksize = 25;
-        while (currentIndex < list.size()) {
-            lnList.addAll(list.subList(
-                    currentIndex,
-                    Math.min(currentIndex + chunksize, list.size()))
-            );
-            currentIndex += chunksize;
-            notifyDataSetChanged();
-        }
     }
 
     class LNTitleViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnCreateContextMenuListener, PopupMenu.OnMenuItemClickListener {
@@ -177,83 +152,8 @@ public class LNShowcaseAdapter extends RecyclerView.Adapter<LNShowcaseAdapter.LN
     }
 
     @Override
-    public void onBindViewHolder(@NonNull LNTitleViewHolder lnTitleViewHolder, int position) {
-        lnTitleViewHolder.initAt(position);
-    }
-
-    @Override
-    public int getItemCount() {
-        return lnList.size();
-    }
-
-    void filterLNList(String keyword,
-                      String type,
-                      String status,
-                      String[] genres) {
-        List<LightNovel> filteredList = new ArrayList<>();
-        for (LightNovel ln : _lnList) {
-            boolean ok = true;
-            // Keyword
-            if (!keyword.equals("") && !ln.getTitle().toLowerCase().contains(keyword.toLowerCase()))
-                continue;
-            // Type
-            switch (type) {
-                case "Chính thức":
-                    if (!ln.getType().equals(LightNovel.ProjectType.OFFICIAL))
-                        ok = false;
-                    break;
-                case "Teaser":
-                    if (!ln.getType().equals(LightNovel.ProjectType.TEASER))
-                        ok = false;
-                    break;
-                case "OLN":
-                    if (!ln.getType().equals(LightNovel.ProjectType.OLN))
-                        ok = false;
-                    break;
-                default:
-                    // Any
-                    break;
-            }
-            if (!ok) continue;
-            // Status
-            switch (status) {
-                case "Active":
-                    if (!ln.getStatus().equals(LightNovel.ProjectStatus.ACTIVE))
-                        ok = false;
-                    break;
-                case "Idle":
-                    if (!ln.getStatus().equals(LightNovel.ProjectStatus.IDLE))
-                        ok = false;
-                    break;
-                case "Stalled":
-                    if (!ln.getStatus().equals(LightNovel.ProjectStatus.STALLED))
-                        ok = false;
-                    break;
-                case "Completed":
-                    if (!ln.getStatus().equals(LightNovel.ProjectStatus.COMPLETED))
-                        ok = false;
-                    break;
-                case "Inactive":
-                    if (!ln.getStatus().equals(LightNovel.ProjectStatus.INACTIVE))
-                        ok = false;
-                    break;
-                default:
-                    // Any
-                    break;
-            }
-            if (!ok) continue;
-            // Genres
-            if (genres.length > 0) {
-                if (!ln.getGenres().containsAll(Arrays.asList(genres))) continue;
-            }
-            // Acceptance
-            filteredList.add(ln);
-        }
-        // Show
-        loadByChunk(filteredList);
-    }
-
-    void showAll() {
-        loadByChunk(_lnList);
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int pos) {
+        if (viewHolder instanceof  LNTitleViewHolder)
+            ((LNTitleViewHolder) viewHolder).initAt(pos);
     }
 }
